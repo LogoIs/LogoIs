d177706d905d08216eb7bdb9be65704f8d8f69f7
LogoIs <113313922+LogoIs@users.noreply.github.com>
2022-11-25 15:32:34 +0300

    installer.sh
    Fixed installer

--- installer.sh
+++ installer.sh
@@ -3,15 +3,14 @@ read yn
  if [[ "$yn" == "y" ]]; then
 apt-get update -y
 apt-get install expect -
-apt-get install git -y
-git clone https://github.com/LogoIs/LogoIs/blob/main/exp
-git clone https://github.com/LogoIs/LogoIs/blob/main/vdsconn
+apt-get install wget -y
+cd $PREFIX/bin
+wget https://github.com/LogoIs/LogoIs/blob/main/exp
 chmod 777 exp
+wget https://github.com/LogoIs/LogoIs/blob/main/vdsconn
 chmod 777 vdsconn
-mv exp $HOME/../usr/bin
-mv vdsconn $HOME/../usr/bin
 clear
-echo "use vdsconn to open script later"
+echo "use vdsconn to open script later!"
 sleep 2
 vdsconn
 fi
